50ce045074dd20f06a9ee5609286c18d19efc125
lrod <rleslie1015@gmail.com>
2019-03-17 17:48:22 -0700

    went back and did the article.js from scratch

--- Article/Article.js
+++ Article/Article.js
@@ -1,34 +1,21 @@
-// Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
-
 class Article {
-  constructor(domElement) {
-    // assign this.domElement to the passed in domElement
-    this.domElement = domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton = this.domElement.querySelector(".expandButton");
-    
-    console.log(this.expandButton);
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "Expand";
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener("click", () => this.expandArticle());
+  constructor(funkyArticle){
+  this.funkyArticle = funkyArticle;
+  this.expand = this.funkyArticle.querySelector(".expandButton");
+  console.log(this.expand);
+  this.expand.textContent = "Expand";
+  this.expand.addEventListener("click", this.showStuff.bind(this));
   }
-
-  expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
-      console.log("i was clicked!");
-      this.domElement.classList.toggle("article-open");
-
+  showStuff () {
+    this.funkyArticle.classList.toggle("article-open");
   }
 }
 
-// START HERE: 
 
-// Select all classes named ".article" and assign that value to the articles variable.  
 
-let articles = document.querySelectorAll('.article');
 
-// With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
-articles.forEach(function(article){
+const allClassesNamedArticles = document.querySelectorAll(".article");
+
+allClassesNamedArticles.forEach(function(article){
   return new Article(article);
 });
